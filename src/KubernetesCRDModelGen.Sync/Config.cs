842f665cfbe8fbcf088ad9c0fc08feeccd73636e
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2022-10-20 23:17:31 -0700

    feat: crd sync

---
+++ src/KubernetesCRDModelGen.Sync/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KubernetesCRDModelGen.Sync
+{
+    internal class Config
+    {
+        public string Group { get; set; }
+
+        public List<string> Urls { get; set; }
+
+        public string HelmRepo { get; set; }
+
+        public string HelmChart { get; set; }
+
+        public bool PreRelease { get; set; }
+    }
+}
