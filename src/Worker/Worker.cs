23da7cbc62c0f3cc5034a991a9fe10cf4d9f17c6
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-02-13 21:20:01 -0800

    wip

--- src/Worker/Worker.cs
+++ src/Worker/Worker.cs
@@ -1,6 +1,12 @@
 using k8s.Models;
 using k8s;
 using KubernetesCRDModelGen;
+using Microsoft.OpenApi.Readers;
+using System.IO;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi;
+using System.Net.Http;
+using System.ComponentModel;
 
 namespace Worker
 {
@@ -15,15 +21,40 @@ namespace Worker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var objects = await KubernetesYaml.LoadAllFromFileAsync("CRD.yaml");
+            //var objects = await KubernetesYaml.LoadAllFromFileAsync("CRD.yaml");
 
-            var crd = objects.First() as V1CustomResourceDefinition;
+            //var crd = objects.First() as V1CustomResourceDefinition;
 
-            var gen = new CRDGenerator();
+            var stream = File.OpenRead("openapi.yaml");
 
-            var code = gen.GenerateCode(crd);
+            var openApiDocument = new OpenApiStreamReader().Read(stream, out var diagnostic);
 
-            var ass = await gen.GenerateAssembly(crd);
+            var stream2 = await new HttpClient().GetStreamAsync("https://raw.githubusercontent.com/kubernetes/kubernetes/v1.25.6/api/openapi-spec/swagger.json");
+
+            // Read V3 as YAML
+            var openApiDocument2 = new OpenApiStreamReader().Read(stream2, out var diagnostic2);
+
+            openApiDocument2.Paths.Clear();
+
+            // Write V2 as JSON
+            var outputString = openApiDocument2.Serialize(OpenApiSpecVersion.OpenApi3_0, OpenApiFormat.Yaml);
+
+            //var gen = new CRDGenerator();
+
+            //var code = gen.GenerateCode(crd);
+
+            //var ass = await gen.GenerateAssembly(crd);
         }
     }
+
+
+    public class IPropertyWrapper<T> : INotifyPropertyChanged
+    {
+        public IPropertyWrapper()
+        {
+            
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+
 }
