fba735eb3127c5b0ac52db5fdbcce01df0a38aa9
IvanJosipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-10-30 14:06:19 +0000

    feat: Update CRDs

--- src/Models/KubernetesCRDModelGen.Models.traefik.io/referencegrants-gateway-networking-k8s-io.g.cs
+++ src/Models/KubernetesCRDModelGen.Models.traefik.io/referencegrants-gateway-networking-k8s-io.g.cs
@@ -12,15 +12,15 @@ namespace KubernetesCRDModelGen.Models.gateway.networking.k8s.io;
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen.Tool", "1.0.0.0"), global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1ReferenceGrantSpecFrom
 {
-    /// <summary>Group is the group of the referent. When empty, the Kubernetes core API group is inferred.   Support: Core</summary>
+    /// <summary>Group is the group of the referent. When empty, the Kubernetes core API group is inferred.  Support: Core</summary>
     [JsonPropertyName("group")]
     public string Group { get; set; }
 
-    /// <summary>Kind is the kind of the referent. Although implementations may support additional resources, the following types are part of the "Core" support level for this field.   When used to permit a SecretObjectReference:   * Gateway   When used to permit a BackendObjectReference:   * GRPCRoute * HTTPRoute * TCPRoute * TLSRoute * UDPRoute</summary>
+    /// <summary>Kind is the kind of the referent. Although implementations may support additional resources, the following types are part of the "Core" support level for this field.  When used to permit a SecretObjectReference:  * Gateway  When used to permit a BackendObjectReference:  * GRPCRoute * HTTPRoute * TCPRoute * TLSRoute * UDPRoute</summary>
     [JsonPropertyName("kind")]
     public string Kind { get; set; }
 
-    /// <summary>Namespace is the namespace of the referent.   Support: Core</summary>
+    /// <summary>Namespace is the namespace of the referent.  Support: Core</summary>
     [JsonPropertyName("namespace")]
     public string Namespace { get; set; }
 }
@@ -29,11 +29,11 @@ public partial class V1beta1ReferenceGrantSpecFrom
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen.Tool", "1.0.0.0"), global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1ReferenceGrantSpecTo
 {
-    /// <summary>Group is the group of the referent. When empty, the Kubernetes core API group is inferred.   Support: Core</summary>
+    /// <summary>Group is the group of the referent. When empty, the Kubernetes core API group is inferred.  Support: Core</summary>
     [JsonPropertyName("group")]
     public string Group { get; set; }
 
-    /// <summary>Kind is the kind of the referent. Although implementations may support additional resources, the following types are part of the "Core" support level for this field:   * Secret when used to permit a SecretObjectReference * Service when used to permit a BackendObjectReference</summary>
+    /// <summary>Kind is the kind of the referent. Although implementations may support additional resources, the following types are part of the "Core" support level for this field:  * Secret when used to permit a SecretObjectReference * Service when used to permit a BackendObjectReference</summary>
     [JsonPropertyName("kind")]
     public string Kind { get; set; }
 
@@ -46,16 +46,16 @@ public partial class V1beta1ReferenceGrantSpecTo
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen.Tool", "1.0.0.0"), global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1ReferenceGrantSpec
 {
-    /// <summary>From describes the trusted namespaces and kinds that can reference the resources described in "To". Each entry in this list MUST be considered to be an additional place that references can be valid from, or to put this another way, entries MUST be combined using OR.   Support: Core</summary>
+    /// <summary>From describes the trusted namespaces and kinds that can reference the resources described in "To". Each entry in this list MUST be considered to be an additional place that references can be valid from, or to put this another way, entries MUST be combined using OR.  Support: Core</summary>
     [JsonPropertyName("from")]
     public IList<V1beta1ReferenceGrantSpecFrom> From { get; set; }
 
-    /// <summary>To describes the resources that may be referenced by the resources described in "From". Each entry in this list MUST be considered to be an additional place that references can be valid to, or to put this another way, entries MUST be combined using OR.   Support: Core</summary>
+    /// <summary>To describes the resources that may be referenced by the resources described in "From". Each entry in this list MUST be considered to be an additional place that references can be valid to, or to put this another way, entries MUST be combined using OR.  Support: Core</summary>
     [JsonPropertyName("to")]
     public IList<V1beta1ReferenceGrantSpecTo> To { get; set; }
 }
 
-/// <summary>ReferenceGrant identifies kinds of resources in other namespaces that are trusted to reference the specified kinds of resources in the same namespace as the policy.   Each ReferenceGrant can be used to represent a unique trust relationship. Additional Reference Grants can be used to add to the set of trusted sources of inbound references for the namespace they are defined within.   All cross-namespace references in Gateway API (with the exception of cross-namespace Gateway-route attachment) require a ReferenceGrant.   ReferenceGrant is a form of runtime verification allowing users to assert which cross-namespace object references are permitted. Implementations that support ReferenceGrant MUST NOT permit cross-namespace references which have no grant, and MUST respond to the removal of a grant by revoking the access that the grant allowed.</summary>
+/// <summary>ReferenceGrant identifies kinds of resources in other namespaces that are trusted to reference the specified kinds of resources in the same namespace as the policy.  Each ReferenceGrant can be used to represent a unique trust relationship. Additional Reference Grants can be used to add to the set of trusted sources of inbound references for the namespace they are defined within.  All cross-namespace references in Gateway API (with the exception of cross-namespace Gateway-route attachment) require a ReferenceGrant.  ReferenceGrant is a form of runtime verification allowing users to assert which cross-namespace object references are permitted. Implementations that support ReferenceGrant MUST NOT permit cross-namespace references which have no grant, and MUST respond to the removal of a grant by revoking the access that the grant allowed.</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen.Tool", "1.0.0.0"), global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 [KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
 public partial class V1beta1ReferenceGrant : IKubernetesObject<V1ObjectMeta>, ISpec<V1beta1ReferenceGrantSpec>
