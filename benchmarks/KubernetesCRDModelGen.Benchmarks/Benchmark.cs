d57b9976b96392a97a4392c3e3919849f97077e4
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-01-19 10:59:08 -0800

    fix: GenerateAssembly (#963)

--- benchmarks/KubernetesCRDModelGen.Benchmarks/Benchmark.cs
+++ benchmarks/KubernetesCRDModelGen.Benchmarks/Benchmark.cs
@@ -19,12 +19,20 @@ public class Benchmark
 
         generator = new Generator(fac);
 
-        crd = KubernetesYaml.LoadFromFileAsync<V1CustomResourceDefinition>("managedclusters.containerservice.azure.com.yaml").Result;
+        crd = KubernetesYaml.LoadFromFileAsync<V1CustomResourceDefinition>("managedclusters.containerservice.azure.com.yaml").GetAwaiter().GetResult();
     }
 
     [Benchmark]
     public void Test1()
     {
+        if (crd == null)
+        {
+            throw new Exception("CRD NULL");
+        }
         var ass = generator!.GenerateAssembly(crd);
+        if (ass.Item1 == null || ass.Item2 == null)
+        {
+            throw new Exception("Failed to generate assembly");
+        }
     }
 }
