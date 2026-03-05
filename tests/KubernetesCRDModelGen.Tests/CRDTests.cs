8238a75832175802102b24a79bc859a4100fa1c3
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-03-04 18:21:52 -0800

    fix: update Microsoft.OpenApi

---
+++ tests/KubernetesCRDModelGen.Tests/CRDTests.cs
@@ -0,0 +1,31 @@
+﻿using k8s;
+using k8s.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KubernetesCRDModelGen.Tests;
+
+public class CRDTests
+{
+
+    [Fact]
+    public async Task TestNamespace()
+    {
+        var fac = new LoggerFactory();
+
+        var generator = new Generator(fac);
+        var crd = KubernetesYaml.LoadFromFileAsync<V1CustomResourceDefinition>("managedclusters.containerservice.azure.com.yaml").GetAwaiter().GetResult();
+
+
+        var ass = generator.GenerateAssembly(crd);
+        if (ass.Item1 == null || ass.Item2 == null)
+        {
+            throw new Exception("Failed to generate assembly");
+        }
+    }
+}
